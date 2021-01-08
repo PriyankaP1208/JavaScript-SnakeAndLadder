249a46b5c9de46debe716e32d0b5bbba7d7c1596
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-01-08 23:51:58 +0530

    Added:Repeat till the player reaches the winning position

--- utility.js
+++ utility.js
@@ -2,25 +2,30 @@ class SnakeAndLadder{
     snakeAndLadder()
     {
         let position=0;
-        let option;
-        let die=Math.floor(Math.random()*6)+1;
-        console.log("Starts with position 0");
-        console.log("Rolls the die:",die);
-        console.log("1.No play 2.Ladder 3.Snake");
-        option=Math.floor(Math.random()*3)+1;
-        switch(option)
+        while(position!=10)
         {
-            case 1:
-                console.log("No Play.");
-                break;
-            case 2:
-                position+=die;
-                console.log("Ladder");
-                break;
-            case 3:
-                position-=die;
-                console.log("Snake");
-                break;
+            let die=Math.floor(Math.random()*6)+1;
+            console.log("Starts with position 0");
+            console.log("Rolls the die:",die);
+            console.log("1.No play 2.Ladder 3.Snake");
+            let option=Math.floor(Math.random()*3)+1;
+            switch(option)
+			{
+				case 1:
+						console.log("No Play.");
+						break;
+				case 2:
+						position+=die;
+						console.log("ladder");
+						break;
+				case 3:
+					    console.log("Snake");
+						if((position-die)<0)
+							position=0;
+						else
+							position-=die;
+						break;
+			}
         }
     }
 }
