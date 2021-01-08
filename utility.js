c153cceecd205ed59e2c7df1d9c638da937b875f
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-01-08 22:44:33 +0530

    Added:Checks for option-No play,Ladder or Snake

--- utility.js
+++ utility.js
@@ -2,9 +2,26 @@ class SnakeAndLadder{
     snakeAndLadder()
     {
         let position=0;
+        let option;
         let die=Math.floor(Math.random()*6)+1;
         console.log("Starts with position 0");
         console.log("Rolls the die:",die);
+        console.log("1.No play 2.Ladder 3.Snake");
+        option=Math.floor(Math.random()*3)+1;
+        switch(option)
+        {
+            case 1:
+                console.log("No Play.");
+                break;
+            case 2:
+                position+=die;
+                console.log("Ladder");
+                break;
+            case 3:
+                position-=die;
+                console.log("Snake");
+                break;
+        }
     }
 }
 module.exports=new SnakeAndLadder();
