b61b6b666ce4e3e8e32dd3dcbdcf0b95cf8335a3
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-01-08 22:29:34 +0530

    Added:Player rolls the die to get number

--- utility.js
+++ utility.js
@@ -2,7 +2,9 @@ class SnakeAndLadder{
     snakeAndLadder()
     {
         let position=0;
+        let die=Math.floor(Math.random()*6)+1;
         console.log("Starts with position 0");
+        console.log("Rolls the die:",die);
     }
 }
 module.exports=new SnakeAndLadder();
