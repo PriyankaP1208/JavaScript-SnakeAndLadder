a5b00e799aed34559061a19c4ec26266f5a2c4e0
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-01-09 09:22:07 +0530

    Added:The no of times the dice was played

--- utility.js
+++ utility.js
@@ -1,14 +1,15 @@
 class SnakeAndLadder{
     snakeAndLadder()
     {
-        let position=0;
+		let position=0;
+		let flag=0;
         while(position!=10)
         {
             let die=Math.floor(Math.random()*6)+1;
-            console.log("Starts with position 0");
             console.log("Rolls the die:",die);
             console.log("1.No play 2.Ladder 3.Snake");
-            let option=Math.floor(Math.random()*3)+1;
+			let option=Math.floor(Math.random()*3)+1;
+			flag++;
             switch(option)
 			{
 				case 1:
@@ -18,7 +19,13 @@ class SnakeAndLadder{
 						position+=die;
 						console.log("ladder");
 						if(position<=10)
+						{
 							position+=die;
+						}
+						else if(position>10)
+						{
+							position=10
+						}
 						break;
 				case 3:
 					    console.log("Snake");
@@ -27,7 +34,11 @@ class SnakeAndLadder{
 						else
 							position-=die;
 						break;
+				default:
+						console.log("Wrong choice");
 			}
+			console.log("Dice Rolled:",flag);
+			console.log("Position:",position);
         }
     }
 }
